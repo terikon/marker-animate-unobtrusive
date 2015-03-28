144f7147dfd6d8a0d807cb60cbef2c549632ce9b
viskin <r.viskin@terikon.com>
2015-03-28 18:15:27 +0300

    Added MarkerWithGhost tests.

---
+++ tests/spec/MarkerWithGhostSpec.js
@@ -0,0 +1,35 @@
+﻿describe("MarkerWithGhost", function () {
+
+    describe("google.maps.Marker", function () {
+
+        describe("before calling initializeGlobally()", function () {
+
+            it("should differ from MarkerWithGhost", function () {
+                expect(google.maps.Marker).not.toBe(MarkerWithGhost);
+            });
+
+        });
+
+        describe("after calling initializeGlobally()", function () {
+
+            var originalMarker;
+            beforeEach(function () {
+                originalMarker = google.maps.Marker;
+
+                MarkerWithGhost.initializeGlobally();
+            });
+
+            it("should be replaced with MarkerWithGhost", function () {
+                expect(google.maps.Marker).toBe(MarkerWithGhost);
+            });
+
+            afterEach(function () {
+                google.maps.Marker = originalMarker;
+            });
+
+        });
+
+    });
+
+    
+});
